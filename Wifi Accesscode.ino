096440fe94fc4d0b620437f2aa17c71e198c7a11
SharmiBonam <133389665+SharmiBonam@users.noreply.github.com>
2023-05-14 12:31:34 -0500

    Create Wifi Accesscode.ino

---
+++ Wifi Accesscode.ino
@@ -0,0 +1,33 @@
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+
+#define LED_BUILTIN 2   // Set the GPIO pin where you connected your test LED or comment this line out if your dev board has a built-in LED
+
+// Set these to your desired credentials.
+const char *ssid = "707";
+const char *password = "Marimba";
+
+WiFiServer server(80);
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Configuring access point...");
+
+  // You can remove the password parameter if you want the AP to be open.
+  WiFi.softAP(ssid);
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.begin();
+
+  Serial.println("Server started");
+}
+
+void loop() {
+ 
+}
